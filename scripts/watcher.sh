1e5cd2bddbf9260e94b2cb9f24f69e0f09333b6d
CJ Pais <cj@cjpais.com>
2020-10-20 12:08:52 -0700

    Updating.... 🍡

--- scripts/watcher.sh
+++ scripts/watcher.sh
@@ -1,4 +1,4 @@
-fswatch -xnr -e ".*" -i ".md" ../serve/day | while read file event; do 
+fswatch -xnr -e ".*" -i ".md" ../serve | while read file event; do 
   echo $file $event
   python3 gen_html.py $file
   python3 gen_index.py
---
+++ serve/day/oct20_2020/index.md
@@ -0,0 +1,4 @@
+# Tuesday October 20th, 2020
+
+A lot happened this weekend. Would like to cover it all again, but this will likely be a separate
+thing.
